c4a2426ce877aebf061d58a18cc61cfebe836d9f
SerhiiRodin <rodinserj@ukr.net>
2023-03-25 22:26:30 +0200

    add some

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import css from './Container/Container.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+
+    // id: nanoid(),
+  };
+
+  addContact = contact => {
+    console.log(contact);
+  };
+
+  render() {
+    return (
+      <div className={css.container}>
+        <h1>Phonebook</h1>
+        <ContactForm addContact={this.addContact} />
+
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
